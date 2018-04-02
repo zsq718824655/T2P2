b2e7e0a766b8e5319136938ff1df39ca83f5a987
55 <55>
2018-04-02 10:27:43 +0800

    ZSQ

--- AppInfoSystem/src/main/java/cn/appsys/interceptor/LoginInterceptor.java
+++ AppInfoSystem/src/main/java/cn/appsys/interceptor/LoginInterceptor.java
@@ -14,7 +14,7 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
 			throws Exception {
 		
 		String requestURI = request.getRequestURI();
-		if(requestURI.indexOf("login")==-1&&requestURI.indexOf("Login")==-1&&requestURI.indexOf("register")==-1&&requestURI.indexOf("Login")==-1&&requestURI.indexOf("queryByUser")==-1) {//请求的的进行验证拦截
+		if(requestURI.indexOf("login")==-1&&requestURI.indexOf("Login")==-1&&requestURI.indexOf("register")==-1&&requestURI.indexOf("Login")==-1&&requestURI.indexOf("queryByUser")==-1&&requestURI.indexOf("todevRegistView")==-1) {//请求的的进行验证拦截
 			Object obj = request.getSession().getAttribute(Constants.USER_SESSION);
 			if(obj==null) {
 				response.sendRedirect(request.getContextPath()+"/Login");
